9813b1d1043d5d8a77f8e5bbe96532d166d2a562
MYushkov <M.Yushkov@aladdin.ru>
2026-03-28 12:53:02 +0300

    Флеш вроде допилил, но не проверил

--- max_hal_stm32f103.c
+++ max_hal_stm32f103.c
@@ -20,7 +20,12 @@
 #include <stdlib.h>
 #include "template_config.h"
 
-// ===== EEPROM =====
+// ===== FLASH =====
+
+/* 1. Чтение можно по байтам
+ * 2. Запись по 2 байта выровнено
+ * 3. Записать можно из 0xFFFF -> 0xXXXX  и 0xXXXX -> 0x0000. Другие комбинации не пишутся
+ */
 
 static void _Flash_Unlock(void)
 {
@@ -40,35 +45,39 @@ static void _Flash_ErasePage(uint32_t page_address)
 {
 	__disable_irq();
 	_Flash_Unlock();
-	while (FLASH->SR & FLASH_SR_BSY);
 	FLASH->CR |= FLASH_CR_PER;
 	FLASH->AR = page_address;
 	FLASH->CR |= FLASH_CR_STRT;
 	while (FLASH->SR & FLASH_SR_BSY);
 	FLASH->CR &= ~FLASH_CR_PER;
-	FLASH->CR |= FLASH_CR_LOCK;
 	_Flash_Lock();
 	__enable_irq();
 }
 
-static int _Flash_Write(uint32_t address, const uint8_t *data, uint32_t size_b)
+// Адрес и размер должны быть выровнены по 2 байта. выход за пределы сектора не проверяется. Размер в байтах внутри переводится в полуслова.
+static int _Flash_Write(uint32_t address_in, const void *data_in, uint32_t size)
 {
-    ASSERT_DEBUG((address & 1) != 0);
+    ASSERT_DEBUG(address_in & 1);
+    ASSERT_DEBUG((uint32_t)data_in & 1);
+    ASSERT_DEBUG(size & 1);
+    size >>= 1;
+    uint16_t *data = (uint16_t *)data_in;
+    __IO uint16_t *address = (__IO uint16_t*)address_in;
+
     _Flash_Unlock();
 
-    while(FLASH->SR & FLASH_SR_BSY);
     FLASH->CR |= FLASH_CR_PG;
-
-    while(size_b)
+    while(size--)
     {
-    	uint16_t hword = ((size_b >= 2 ? data[1] : 0) << 8) | data[0];
-    	__disable_irq();
-    	* (__IO uint16_t*)address = hword;
-    	while(FLASH->SR & FLASH_SR_BSY);
-    	__enable_irq();
-    	address += 2;
-    	data += 2;
-    	size_b -= 2;
+    	if(*data != *address)
+    	{
+    		__disable_irq();
+    		*address = *data;
+    		while(FLASH->SR & FLASH_SR_BSY);
+    		__enable_irq();
+    	}
+    	address++;
+    	data++;
     }
     FLASH->CR &= ~FLASH_CR_PG;// Выключаем режим программирования после завершения цикла
     _Flash_Lock();// Финальная блокировка
@@ -96,27 +105,46 @@ void Flash_Read(uint32_t handler, uint16_t offset, void *data, uint16_t size)
 	memcpy(data, ptr, size);
 }
 
-void Flash_Write_data(uint32_t handler, uint16_t offset, void* data_in, uint16_t size_byte)
+// Всё в границах одного сектора. Пока только для Param, надо будет-расширишь
+void Flash_Write_data(uint32_t handler, uint16_t offset, void* data_in, uint16_t size_b)
 {
+
 	uint16_t hoffset = handler >> 16;
 	uint16_t hsize = handler;
-	ASSERT_DEBUG(offset + size_byte > hsize);
-	ASSERT_DEBUG(hoffset + offset + size_byte > PARAM_FLASH_SIZE);
+	ASSERT_DEBUG(offset + size_b > hsize);
+	ASSERT_DEBUG(hoffset + offset + size_b > PARAM_FLASH_SIZE);
 	ASSERT_DEBUG(!data_in);
 	ASSERT_DEBUG(hoffset & 3);
 	ASSERT_DEBUG(hsize & 3);
-/// Надо не стирать если fffff
-	uint8_t *ptr_flash = (uint8_t*)PARAM_FLASH_BEGIN + hoffset;
-	if ( memcmp(ptr_flash, data_in, size_byte) != 0)
+
+	uint16_t *ptr_flash = (uint16_t*)PARAM_FLASH_BEGIN + (hoffset >> 1);
+	// Проверяем, надо ли писать вобще
+	if ( !memcmp(ptr_flash, data_in, size_b))
 	{
-		uint8_t *sector = malloc(PARAM_FLASH_SIZE);
-		ASSERT_DEBUG(!sector);
-		memcpy(sector, (void *)PARAM_FLASH_BEGIN, PARAM_FLASH_SIZE);
-		memcpy(sector + hoffset + offset, data_in, size_byte);
-		_Flash_ErasePage(PARAM_FLASH_BEGIN);
-		_Flash_Write(PARAM_FLASH_BEGIN, sector, PARAM_FLASH_SIZE);
-		free(sector);
+		return;
 	}
+	// Не стал колдовать с выравниваниями, проверками границ data и flash. Простой но немного более накладный путь - сразу выделяю сектор и только потом смотрю, надо ли стирать. Всё равно запись гораздо дольше чем вся эта возня
+	uint16_t *sector = malloc(PARAM_FLASH_SIZE);
+	ASSERT_DEBUG(!sector);
+	memcpy(sector, (void *)PARAM_FLASH_BEGIN, PARAM_FLASH_SIZE);
+	memcpy(sector + hoffset + offset, data_in, size_b);
+	// Теперь в sector то, что должно быть на флеше. Проверяем, надо ли стирать.
+	for (int pos = 0; pos < (PARAM_FLASH_SIZE >> 1); pos++)
+	{
+		uint16_t onflash = ((uint16_t *)PARAM_FLASH_BEGIN)[pos];
+		if( (onflash == 0xFFFF) || (onflash == sector[pos]) || (sector[pos] == 0) )
+		{
+			continue;
+		}
+		_Flash_ErasePage(PARAM_FLASH_BEGIN);
+		break;
+	}
+	_Flash_Write(PARAM_FLASH_BEGIN, sector, PARAM_FLASH_SIZE);
+
+	ASSERT_DEBUG(memcmp(sector, (void*)PARAM_FLASH_BEGIN, PARAM_FLASH_SIZE));
+	free(sector);
+
+
 }
 
 volatile uint32_t cnt = 0;
@@ -132,9 +160,24 @@ uint32_t MaxHal_CheckFlash(uint32_t addr, uint32_t sector_size, uint32_t sectors
 	uint32_t handler = (4 << 16) | 4;
 	uint32_t abc = 0xDEADBEEF;
 	Flash_Write_data(handler, 0, &abc, 4);
+
+
+	_Flash_ErasePage(addr);
+	uint16_t val = 0xFFFE;
+	_Flash_Write(addr, &val, sizeof(val));
+	val = 0x0000;
+	_Flash_Write(addr + 2, &val, sizeof(val));
+	val = 0xFEFE;
+	_Flash_Write(addr, &val, sizeof(val));
+	val = 0xFFFF;
+	_Flash_Write(addr, &val, sizeof(val));
+
+	return;
 */
 	ASSERT_DEBUG(sector_size & (sector_size - 1));
 	ASSERT_DEBUG(addr & (sector_size - 1));
+
+
 
 	uint8_t *buff = malloc(sector_size);
 	ASSERT_DEBUG(!buff);
